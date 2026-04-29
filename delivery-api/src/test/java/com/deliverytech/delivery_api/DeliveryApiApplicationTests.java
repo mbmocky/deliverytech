d9fd089935fe16c82a6826c0ff8003299eaf39be
Moises Borges <mbmocky@bol.com.br>
2026-04-29 20:18:36 -0300

    git commit

--- delivery-api/src/test/java/com/deliverytech/delivery_api/DeliveryApiApplicationTests.java
+++ delivery-api/src/test/java/com/deliverytech/delivery_api/DeliveryApiApplicationTests.java
@@ -2,7 +2,7 @@ package com.deliverytech.delivery_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/* Teste de Commit */
 @SpringBootTest
 class DeliveryApiApplicationTests {
 
